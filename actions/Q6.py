7a9f8436192c5b07f9daefce3a5de86bd5a0413a
israks <nayime@hotmail.fr>
2023-11-30 22:52:11 +0100

    ninth commit

--- actions/Q6.py
+++ actions/Q6.py
@@ -11,14 +11,14 @@ class Window(tk.Toplevel):
 
         # Définition de la taille de la fenêtre, du titre et des lignes/colonnes de l'affichage grid
         display.centerWindow(1000, 600, self)
-        self.title('Q6 : graphique sur la corrélation températures minimales - coût de travaux (Isère / 2022)')
+        self.title('Q6 : graphique sur la corrélation températures minimales - coût de travaux (Isère / 2018)')
         display.defineGridDisplay(self, 1, 1)
 
         query = """
-            SELECT strftime('%m', Mesures.date_mesure) as mois, ROUND(AVG(temperature_min_mesure), 2) as moy_min, SUM(cout_total_ht_travaux) AS tot_couts
-            FROM Mesures JOIN Departements USING (code_departement) JOIN Travaux USING (code_region)
+            SELECT strftime('%m', date_mesure) as mois, AVG(temperature_min_mesure) as moymin
+            FROM Mesures
+            WHERE code_departement = 38 AND strftime('%Y', date_mesure) = '2018'
             GROUP BY mois
-            HAVING nom_departement = 'ISERE' AND strftime('%Y', Meusres.date_mesure) = 2022
         """
 
         # Extraction des données et affichage dans le tableau
@@ -30,35 +30,64 @@ class Window(tk.Toplevel):
             print("Erreur : " + repr(e))
 
         # Extraction et préparation des valeurs à mettre sur le graphique
-        tabmois = []
+        tabmois_temp = []
         tabmin = []
-        tabcouts = []
-
 
         for row in result:
+            tabmois_temp.append(row[0])
             tabmin.append(row[1])
-            tabmois.append(row[0])
-            tabcouts.append(row[2])
 
-        # Formatage des dates pour l'affichage sur l'axe x
-        datetime_dates = [datetime.strptime(date, '%m') for date in tabmois]
+        datetime_dates = [datetime.strptime(date, '%m') for date in tabmois_temp]
 
-        # Ajout de la figure et du subplot qui contiendront le graphique
+        query2 = """
+            SELECT strftime('%m', date_travaux) as mois, SUM(cout_total_ht_travaux) as total_cout_travaux
+            FROM Travaux JOIN Departements USING (code_region)
+            WHERE code_departement = 75 AND strftime('%Y', date_travaux) = '2018'
+            GROUP BY mois
+        """
+
+        # Extraction des données et affichage dans le tableau
+        result2 = []
+        try:
+            cursor = db.data.cursor()
+            cursor.execute(query2)
+            result2 = cursor.fetchall()
+
+            print("Résultats de la requête 2:")
+            for row in result2:
+                print(f"Mois: {row[0]}, Total Cout Travaux: {row[1]}")
+
+        except Exception as e:
+            print("Erreur : " + repr(e))
+
+        tabmois_travaux = []
+        tabcouts = []
+
+        for rows in result2:
+            tabmois_travaux.append(rows[0])
+            tabcouts.append(rows[1])
+
         fig = Figure(figsize=(10, 6), dpi=100)
         plot1 = fig.add_subplot(111)
 
-        # Affichage des courbes
-        plot1.plot(range(len(datetime_dates)), tabmin, color='b', label='moy des temp. min')
-        plot1.plot(range(len(datetime_dates)), tabcouts, color='r', label='total des couts')
+        # Plot des températures minimales moyennes
+        plot1.plot(range(len(datetime_dates)), tabmin, color='red', label='temp min moyenne')
+        # Ajout de la deuxième série de données sur le même subplot
+        plot1.plot(range(len(datetime_dates)), tabcouts, color='green', label='cout total travaux')
 
-        # Configuration de l'axe x pour n'afficher que le numéro du mois
-        xticks = [i for i, date in enumerate(datetime_dates) if date.day == 1]
-        xticklabels = [date.strftime('%m') for date in datetime_dates if date.day == 1]
-        plot1.set_xticks(xticks)
-        plot1.set_xticklabels(xticklabels, rotation=45)
+        plot1.set_xticks(range(len(datetime_dates)))
+        plot1.set_xticklabels(tabmois_temp, rotation=45, ha="right")
+
+        plot1.set_xlabel('mois')
+        plot1.set_ylabel('Moyenne de la température miniminum')
+
         plot1.legend()
 
         # Affichage du graphique
-        canvas = FigureCanvasTkAgg(fig,  master=self)
+        canvas = FigureCanvasTkAgg(fig, master=self)
         canvas.draw()
         canvas.get_tk_widget().pack()
+
+        print(tabmois_temp)
+        print(tabmin)
+        print(tabcouts)
